d104bf2df34cc98cdcd711f4b3a5490b7f268451
abdou fk <abdourrhamanefarkhsi@gmail.com>
2023-06-19 05:14:56 +0200

    Modification de l'appel à la fonction generateFilter

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -1,127 +1,74 @@
-// import {recipesData, recipes} from '../index.js'
-
-// // V1 FONCTIONNELLE
-// function searchData() {
-//     const searchBar = document.querySelector(".search_zone_input")
-//     const defaultMessage = document.querySelector(".message-no-match-result")
-//     const cards = document.querySelectorAll('.cards')
-
-
-//     // console.log('affichage des cards', cards)
-
-//     searchBar.addEventListener("keyup", (event)=>{
-//         const searchedName = event.target.value.toLowerCase()
-//         const cardsLength = cards.length
-
-//         cards.forEach(card => {
-//             const isVisible = card.textContent.toLowerCase().includes(searchedName)
-//             const cardVerif = false
-//             if(searchedName.length > 2 && !isVisible) {
-//                 card.classList.add("hide")
-//                 card.style.display= 'none'
-//             } else {         
-//                 card.classList.remove("hide")
-//                 card.style.display= 'block'
-//             }
-//         })
-
-//         const cardsHiddenLength = document.getElementsByClassName('hide').length
-//         if(cardsLength === cardsHiddenLength) {
-//             // console.log('ca filtre!', cardsLength, cardsHiddenLength)
-//             defaultMessage.innerHTML = `Aucune recette ne contient ‘${searchedName}’ vous pouvez chercher «
-//             tarte aux pommes », « poisson », etc. `
-//         } else {
-//             // console.log('ca supprime!', cardsLength, cardsHiddenLength)
-//             defaultMessage.innerHTML = ""
-//         }
-//     })
-// }
-
+const searchBar = document.querySelector(".search_zone_input")
 
 //V2 FONCTIONNELLE
-/*export*/ function searchData() {
-    const searchBar = document.querySelector(".search_zone_input")
+function searchData() {
     const defaultMessage = document.querySelector(".message-no-match-result")
     const clearSearch = document.querySelector(".search_zone_clear")
-
+    // let recipesDefault= recipes
     let newRecipes = [];
     let tagIsVisible = false
 
-    /* ajout du listener de comparaison à la saisie dans l'input */
-    searchBar.addEventListener("keyup", (event)=>{
-        let searchedName = event.target.value;
-        // console.log('saisie clavier:', searchedName)
 
+    let searchedName ;
+    if(searchBar.value.length > 2) {
+        searchedName = searchBar.value
         const regexInput = new RegExp (`${searchedName.trim().toLowerCase()}`);
-        // console.log('regex', regexInput)
-
-        // let recipesLength = recipes.length
-        // let index = 0
-
-      
-
-        if(searchBar.value.length > 2) {
-            for(let index=0; index < recipes.length; index++) {
-                if(newRecipes.includes(recipes[index])===false) {
-                    let cardIsVisible = false;
-                    if (regexInput.test(recipes[index].name.toLowerCase())){
-                        cardIsVisible=true;
-                    } else if (regexInput.test(recipes[index].description.toLowerCase())) {
-                        cardIsVisible=true;
-                    }
-                    for (let i = 0; i < recipes[index].ingredients.length; i++) {
-                        if (regexInput.test(recipes[index].ingredients[i].ingredient.toLowerCase())) {
-                            cardIsVisible=true; 
-                        }
-                    }
-                    if(cardIsVisible===true) {
-                        newRecipes.push(recipes[index])
-                    } 
-                    
+        let index = 0
+        while(index < recipes.length) {
+            if(newRecipes.includes(recipes[index])===false) {
+                let cardIsVisible = false;
+                if (regexInput.test(recipes[index].name.toLowerCase())){
+                    cardIsVisible=true;
+                } else if (regexInput.test(recipes[index].description.toLowerCase())) {
+                    cardIsVisible=true;
                 }
-                
+                for (let i = 0; i < recipes[index].ingredients.length; i++) {
+                    if (regexInput.test(recipes[index].ingredients[i].ingredient.toLowerCase())) {
+                        cardIsVisible=true; 
+                    }
+                }
+                if(cardIsVisible===true) {
+                    newRecipes.push(recipes[index])
+                }                    
             }
-            sum(newRecipes)  
-        }
-        
+        index++
+        sum(newRecipes) 
+        generateFiltersList(newRecipes)
+        }    
+    }
 
-        /** affichage du message par default lorsqu'aucune recette n'est trouvée */
-        if(newRecipes.length>0) {
-            defaultMessage.innerHTML=""
-        } else {
-            defaultMessage.innerHTML = `Aucune recette ne contient ‘${searchBar.value}’ vous pouvez chercher «
-                tarte aux pommes », « poisson », etc. `
-        }
-
-
-        /** nouveau display en fonction de la recherche */
-        if((searchBar.value.length > 2)){
-            recipesData(newRecipes)
-            // console.log("nouveau tableau:" , newRecipes)
-            generateFiltersList(newRecipes)
-            clearSearch.style.display="block"   
-        } else {
-            defaultMessage.innerHTML=""
-            recipesData(recipes)
-            generateFiltersList(recipes)
-            sum(recipes)
-            clearSearch.style.display="none"   
-        }
+    /** affichage du message par default lorsqu'aucune recette n'est trouvée */
+    if(newRecipes.length>0) {
+        defaultMessage.innerHTML=""
+    } else {
+        defaultMessage.innerHTML = `Aucune recette ne contient ‘${searchBar.value}’ vous pouvez chercher «
+            tarte aux pommes », « poisson », etc. `
+    }
 
 
-        const ingredientsElements = document.querySelectorAll(".tags_ingredients_block_content")
-        const ingredientsElementsArray = Array.from(ingredientsElements)
-        console.log("liste des tags actifs:", ingredientsElementsArray)
+    /** nouveau display en fonction de la recherche */
+    if((searchBar.value.length > 2)){
+        recipesData(newRecipes)
+        clearSearch.style.display="block"   
+    } else {
+        defaultMessage.innerHTML=""        
+        recipesData(recipes)
+        sum(recipes)
+        clearSearch.style.display="none"             
+    }
 
-        if(ingredientsElementsArray.length>0) {
-            tagIsVisible=true
-            console.log("test tags visible true",tagIsVisible)
-        } else {
-            tagIsVisible=false
-            console.log("test tags visible false",tagIsVisible)
-        }
 
-    })
+    const ingredientsElements = document.querySelectorAll(".tags_ingredients_block_content")
+    const ingredientsElementsArray = Array.from(ingredientsElements)
+    console.log("liste des tags actifs:", ingredientsElementsArray)
+
+    if(ingredientsElementsArray.length>0) {
+        tagIsVisible=true
+        console.log("test tags visible true",tagIsVisible)
+    } else {
+        tagIsVisible=false
+        console.log("test tags visible false",tagIsVisible)
+    }
 
     /** Fonction reinitialisation input et affichage */
     function clearSearchBar() {
@@ -152,3 +99,7 @@
     searchBtn.onmouseout = searchBtnOut
 }
 
+
+searchBar.addEventListener("keyup", ()=>{
+    searchData()
+})
